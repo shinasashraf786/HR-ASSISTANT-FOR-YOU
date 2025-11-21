9d0d7ef25bc88aa3a1b76dc8f3b5e698fd458ff2
Shinas Ashraf <shinasashraf786@gmail.com>
2025-11-21 10:48:17 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ def authenticate():
             st.error("Invalid credentials")
 
 if "authenticated" not in st.session_state or not st.session_state["authenticated"]:
+    st.title("Login Required")
     authenticate()
     st.stop()
 
